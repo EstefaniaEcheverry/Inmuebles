9c307707ebc0a639f7be0d718b8c7148590c2106
Estefania Echeverry Franco <52355673+EstefaniaEcheverry@users.noreply.github.com>
2022-10-25 14:02:37 -0500

    Actualizar

--- app.R
+++ app.R
@@ -199,10 +199,10 @@ ui <- fluidPage(
                          column(width = 8, tags$img(src="image.png", width =400 , height = 200,alt ="Something went wrong",deleteFile=FALSE),
                                 align = "center"),
                          column(width = 4, tags$br() ,
-                                tags$p(" Son 5769 inmuebles controlados por los diferentes Centros de costos , 
-                                     segun los datos 24 de estos inmuebles estan bloqueados o desactivados,
-                                     es decir, existen 5745 inmuebles activos distribuidos por los centros 
-                                     de costos; Los Colores maneja 1078 de estos inmuebles y Laureles 888
+                                tags$p(" Son 5782 inmuebles controlados por los diferentes Centros de costos , 
+                                     segun los datos 16 de estos inmuebles estan bloqueados o desactivados,
+                                     es decir, existen 5766 inmuebles activos distribuidos por los centros 
+                                     de costos; Los Colores maneja 1081 de estos inmuebles y Laureles 890
                                      inmuebles. ")
                          )
                        )
